188fe691b6beb7694ac49d7ca9d76c66f4c53d7f
Ivo Emanuel Tana <ivo@unimedsuldeminas.coop.br>
2025-07-16 09:15:24 -0300

    Refatorando estrutura de pastas do projeto.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { generateReturnsArray } from "./src/investimentsGoals.js";
+import { generateReturnsArray } from "./js/investimentsGoals.js";
 import { Chart } from "chart.js/auto";
-import { createTable } from "./src/table.js";
+import { createTable } from "./js/table.js";
 
 const finalMoneyChart = document.getElementById("final-money-distribution");
 const progressionChart = document.getElementById("progression");
